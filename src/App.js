1426146f3683646d1a38d4fc91c3f70c39a719ba
LionyxML <rahul.juliato@gmail.com>
2021-12-21 23:33:14 -0300

    Added color pickers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,103 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { Box, InputLabel, TextField, Typography } from "@mui/material";
+import reactCSS from "reactcss";
+import { SketchPicker } from "react-color";
+
 import "./App.css";
 var QRCode = require("qrcode.react");
 
 function App() {
   const [originalString, setOriginalString] = useState("");
+  const [foregroundColor, setForegroundColor] = useState({
+    rgb: {
+      r: 0,
+      g: 0,
+      b: 0,
+      a: 1,
+    },
+  });
+  const [displayFgColorPicker, setDisplayFgColorPicker] = useState(false);
+  const [backgroundColor, setBackroundColor] = useState({
+    rgb: {
+      r: 255,
+      g: 255,
+      b: 255,
+      a: 1,
+    },
+  });
+  const [displayBgColorPicker, setDisplayBgColorPicker] = useState(false);
 
-  const handleGenerate = () => {
-    console.log(originalString);
+  const handleOpenFg = () => {
+    setDisplayFgColorPicker(true);
   };
+
+  const handleCloseFg = () => {
+    setDisplayFgColorPicker(false);
+  };
+
+  const handleFgChange = (color) => {
+    setForegroundColor(color);
+  };
+
+  const handleOpenBg = () => {
+    setDisplayBgColorPicker(true);
+  };
+
+  const handleCloseBg = () => {
+    setDisplayBgColorPicker(false);
+  };
+
+  const handleBgChange = (color) => {
+    setBackroundColor(color);
+  };
+
+  const styles = reactCSS({
+    default: {
+      fgColor: {
+        width: "36px",
+        height: "14px",
+        borderRadius: "2px",
+        background: `rgba(${foregroundColor.rgb.r}, ${foregroundColor.rgb.g}, ${foregroundColor.rgb.b}, ${foregroundColor.rgb.a})`,
+      },
+      bgColor: {
+        width: "36px",
+        height: "14px",
+        borderRadius: "2px",
+        background: `rgba(${backgroundColor.rgb.r}, ${backgroundColor.rgb.g}, ${backgroundColor.rgb.b}, ${backgroundColor.rgb.a})`,
+      },
+      swatch: {
+        padding: "5px",
+        background: "#fff",
+        borderRadius: "1px",
+        boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+        display: "inline-block",
+        cursor: "pointer",
+      },
+      popover: {
+        position: "absolute",
+        zIndex: "2",
+      },
+      cover: {
+        position: "fixed",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
+      },
+    },
+  });
 
   return (
     <div className="App">
-      <Typography variant="h4"> QR-Code Generator </Typography>
+      <Typography
+        variant="h4"
+        sx={{
+          my: 8,
+        }}
+      >
+        {" "}
+        QR-Code Generator{" "}
+      </Typography>
 
       <Box
         sx={{
@@ -42,13 +127,47 @@ function App() {
             value={originalString}
             onChange={(evt) => setOriginalString(evt.target.value)}
           />
+
+          <div>
+            <InputLabel>Foreground</InputLabel>
+            <div style={styles.swatch} onClick={handleOpenFg}>
+              <div style={styles.fgColor} />
+            </div>
+
+            {displayFgColorPicker ? (
+              <div style={styles.popover}>
+                <div style={styles.cover} onClick={handleCloseFg} />
+                <SketchPicker
+                  color={foregroundColor}
+                  onChange={handleFgChange}
+                />
+              </div>
+            ) : null}
+          </div>
+
+          <div>
+            <InputLabel>Background</InputLabel>
+
+            <div style={styles.swatch} onClick={handleOpenBg}>
+              <div style={styles.bgColor} />
+            </div>
+            {displayBgColorPicker ? (
+              <div style={styles.popover}>
+                <div style={styles.cover} onClick={handleCloseBg} />
+                <SketchPicker
+                  color={backgroundColor}
+                  onChange={handleBgChange}
+                />
+              </div>
+            ) : null}
+          </div>
         </Box>
         <Box>
           <QRCode
             value={originalString}
             size={250}
-            bgColor="#FFCCBB"
-            fgColor="#333"
+            bgColor={`rgba(${backgroundColor.rgb.r}, ${backgroundColor.rgb.g}, ${backgroundColor.rgb.b}, ${backgroundColor.rgb.a})`}
+            fgColor={`rgba(${foregroundColor.rgb.r}, ${foregroundColor.rgb.g}, ${foregroundColor.rgb.b}, ${foregroundColor.rgb.a})`}
             level="M"
             includeMargin={true}
           />
