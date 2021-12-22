5981877ed92e76629b9284180e8eac361d5ba4e3
LionyxML <rahul.juliato@gmail.com>
2021-12-21 22:43:55 -0300

    Base app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useRef, useState } from "react";
 import "./App.css";
+var QRCode = require("qrcode.react");
 
 function App() {
-  return <div className="App"></div>;
+  const [originalString, setOriginalString] = useState("");
+
+  const handleGenerate = () => {
+    console.log(originalString);
+  };
+
+  return (
+    <div className="App">
+      <Typography variant="h4"> QR-Code Generator </Typography>
+
+      <Box
+        sx={{
+          my: 8,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gridGap: "40px",
+        }}
+      >
+        <Box
+          sx={{
+            my: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "300px",
+          }}
+        >
+          <TextField
+            label="Type here: "
+            id="message"
+            name="message"
+            margin="normal"
+            value={originalString}
+            onChange={(evt) => setOriginalString(evt.target.value)}
+          />
+        </Box>
+        <Box>
+          <QRCode
+            value={originalString}
+            size={250}
+            bgColor="#FFCCBB"
+            fgColor="#333"
+            level="M"
+            includeMargin={true}
+          />
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
