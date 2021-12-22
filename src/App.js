cac6e5f9ac8c6a40480baabb6923734cf107e9a7
LionyxML <rahul.juliato@gmail.com>
2021-12-22 00:42:04 -0300

    some visual changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import "./App.css";
 var QRCode = require("qrcode.react");
 
 function App() {
-  const PICT_DEFAULT_SIZE = 200;
+  const PICT_DEFAULT_SIZE = 300;
   const PICT_MIN_SIZE = 150;
   const PICT_MAX_SIZE = 400;
   const [originalString, setOriginalString] = useState("");
@@ -31,9 +31,9 @@ function App() {
   const [displayFgColorPicker, setDisplayFgColorPicker] = useState(false);
   const [backgroundColor, setBackroundColor] = useState({
     rgb: {
-      r: 238,
-      g: 238,
-      b: 238,
+      r: 255,
+      g: 255,
+      b: 255,
       a: 1,
     },
   });
@@ -111,107 +111,135 @@ function App() {
 
   return (
     <div className="App">
-      <Typography
-        variant="h4"
-        sx={{
-          my: 8,
-        }}
-      >
-        R-QR Code Generator
-      </Typography>
-
       <Box
         sx={{
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
           gridGap: "40px",
+          height: "500px",
         }}
       >
+        <Box>
+          <Typography
+            variant="h4"
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "1px solid black",
+              borderRadius: "20px",
+              p: 2,
+              mt: 4,
+              backgroundColor: "white",
+              boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.5)",
+              fontFamily: "'Kirang Haerang', cursive; font-size: 4rem",
+            }}
+          >
+            R-QR Code Generator
+          </Typography>
+        </Box>
+
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
             alignItems: "center",
             justifyContent: "center",
-            width: "300px",
+            border: "1px solid black",
+            borderRadius: "20px",
+            padding: "25px",
+            backgroundColor: "white",
+            boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.5)",
           }}
         >
-          <TextField
-            fullWidth
-            label="Text goes here "
-            id="message"
-            name="message"
-            margin="normal"
-            value={originalString}
-            onChange={(evt) => setOriginalString(evt.target.value)}
-          />
-
-          <Slider
-            defaultValue={PICT_DEFAULT_SIZE}
-            min={PICT_MIN_SIZE}
-            max={PICT_MAX_SIZE}
-            value={typeof pictureSize === "number" ? pictureSize : 0}
-            onChange={handlePictureSizeChange}
-            aria-labelledby="input-slider"
-          />
-
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch
-                  label="uhu"
-                  checked={margin}
-                  onChange={handleMarginChange}
-                />
-              }
-              label="Margin"
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "300px",
+              p: 3,
+            }}
+          >
+            <TextField
+              fullWidth
+              label="Text goes here "
+              id="message"
+              name="message"
+              margin="normal"
+              value={originalString}
+              onChange={(evt) => setOriginalString(evt.target.value)}
             />
-          </FormGroup>
-          <div>
-            <InputLabel>Foreground</InputLabel>
-            <div style={styles.swatch} onClick={handleOpenFg}>
-              <div style={styles.fgColor} />
+
+            <Slider
+              defaultValue={PICT_DEFAULT_SIZE}
+              min={PICT_MIN_SIZE}
+              max={PICT_MAX_SIZE}
+              value={typeof pictureSize === "number" ? pictureSize : 0}
+              onChange={handlePictureSizeChange}
+              aria-labelledby="input-slider"
+            />
+
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    label="uhu"
+                    checked={margin}
+                    onChange={handleMarginChange}
+                  />
+                }
+                label="Margin"
+              />
+            </FormGroup>
+            <div>
+              <InputLabel>Foreground</InputLabel>
+              <div style={styles.swatch} onClick={handleOpenFg}>
+                <div style={styles.fgColor} />
+              </div>
+
+              {displayFgColorPicker ? (
+                <div style={styles.popover}>
+                  <div style={styles.cover} onClick={handleCloseFg} />
+                  <SketchPicker
+                    color={foregroundColor}
+                    onChange={handleFgChange}
+                  />
+                </div>
+              ) : null}
             </div>
 
-            {displayFgColorPicker ? (
-              <div style={styles.popover}>
-                <div style={styles.cover} onClick={handleCloseFg} />
-                <SketchPicker
-                  color={foregroundColor}
-                  onChange={handleFgChange}
-                />
+            <div>
+              <InputLabel>Background</InputLabel>
+
+              <div style={styles.swatch} onClick={handleOpenBg}>
+                <div style={styles.bgColor} />
               </div>
-            ) : null}
-          </div>
-
-          <div>
-            <InputLabel>Background</InputLabel>
-
-            <div style={styles.swatch} onClick={handleOpenBg}>
-              <div style={styles.bgColor} />
+              {displayBgColorPicker ? (
+                <div style={styles.popover}>
+                  <div style={styles.cover} onClick={handleCloseBg} />
+                  <SketchPicker
+                    color={backgroundColor}
+                    onChange={handleBgChange}
+                  />
+                </div>
+              ) : null}
             </div>
-            {displayBgColorPicker ? (
-              <div style={styles.popover}>
-                <div style={styles.cover} onClick={handleCloseBg} />
-                <SketchPicker
-                  color={backgroundColor}
-                  onChange={handleBgChange}
-                />
-              </div>
-            ) : null}
-          </div>
-        </Box>
-        <Box>
-          <QRCode
-            value={originalString}
-            size={pictureSize}
-            bgColor={`rgba(${backgroundColor.rgb.r}, ${backgroundColor.rgb.g}, ${backgroundColor.rgb.b}, ${backgroundColor.rgb.a})`}
-            fgColor={`rgba(${foregroundColor.rgb.r}, ${foregroundColor.rgb.g}, ${foregroundColor.rgb.b}, ${foregroundColor.rgb.a})`}
-            level="M"
-            includeMargin={margin}
-          />
+          </Box>
+          <Box>
+            <QRCode
+              value={originalString}
+              size={pictureSize}
+              bgColor={`rgba(${backgroundColor.rgb.r}, ${backgroundColor.rgb.g}, ${backgroundColor.rgb.b}, ${backgroundColor.rgb.a})`}
+              fgColor={`rgba(${foregroundColor.rgb.r}, ${foregroundColor.rgb.g}, ${foregroundColor.rgb.b}, ${foregroundColor.rgb.a})`}
+              level="M"
+              includeMargin={margin}
+            />
+          </Box>
         </Box>
       </Box>
     </div>
